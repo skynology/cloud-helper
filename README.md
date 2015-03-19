6bd40fa1127ff1927b6cd67afe2a9cf991df626c
William <williamyeah@vip.qq.com>
2015-03-19 14:03:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# cloud-helper
-cloud helper for golang
+# 云代码-Helper 
+
+用于云代码中使用的helper. 也就是云代码中传入的参数 `h`. 
+
+写云代码时, 可在本地引入所需的库, 这样可以很好的利用IDE, 当写好后, 把代码copy到控制台发布会更方便.
+
+
+## 代码贡献
+我们由衷欢迎你贡献代码。 让我们一起来努力， 让上空云更加完善强大。 给您带来最好的体验。 您可以直接修改代码， 并push到 `develop` 分支上。
+
+## 联系我们
+
+* 我们的主页: [上空云](https://www.skynology.com)
+* 我的开发者中心: [上空云开发者中心](http://developer.skynology.com)
+* 发送电邮: sdk@skynology.com
+
+## 代码许可
+This SDK is released under the MIT license. See the [LICENSE](https://github.com/skynology/objc-sdk/blob/master/LICENSE) file for more details.
+
---
+++ helper.go
@@ -0,0 +1,81 @@
+package helper
+
+import (
+	"time"
+
+	"github.com/skynology/cloud-types"
+)
+
+// 创建新Helper
+func NewHelper() *Helper {
+	return &Helper{
+		HideFields:    []string{},
+		ProtectFields: []string{},
+		Logs:          []types.CloudLog{},
+		Data:          make(map[string]interface{}),
+	}
+}
+
+// Helper
+type Helper struct {
+	Type          string
+	IsFunction    bool
+	HideFields    []string
+	ProtectFields []string
+	Result        interface{}
+	Data          map[string]interface{}
+	Logs          []types.CloudLog
+	Errors        types.CloudError
+}
+
+// 完成并退出
+// result 参数必须为可转为Json的对象
+func (h *Helper) Render(result interface{}) {
+	// 不允许在表hook中使用
+	if h.Type == "hook" {
+		return
+	}
+	h.Result = result
+}
+
+// 更新字段
+func (h *Helper) Set(fieldName string, value interface{}) {
+	h.Data[fieldName] = value
+}
+
+// 取消并返回
+// 返回 RESTFul Error格式,
+// Code: -1, Error: message
+func (h *Helper) Cancel(message string) {
+	h.Errors = types.CloudError{Code: -1, Message: message}
+}
+
+// 取消并返回
+// 返回 RESTFul Error格式,
+// Code: code, Error: message
+func (h *Helper) CancelWithCode(message string, code int) {
+	h.Errors = types.CloudError{Code: code, Message: message}
+}
+
+// 隐藏字段。不返回给API调用者
+func (h *Helper) Hide(fieldName string) {
+	h.HideFields = append(h.HideFields, fieldName)
+}
+
+// 保护字段， 设置过的字段将无法更新
+func (h *Helper) Protect(fieldName string) {
+	h.ProtectFields = append(h.ProtectFields, fieldName)
+}
+
+// 打印log, 方便调试
+func (h *Helper) Log(message string) {
+	h.LogWithFlag(message, "info")
+}
+
+func (h *Helper) LogWithFlag(message string, flag string) {
+	l := types.CloudLog{}
+	l.Content = message
+	l.CreatedAt = time.Now().Format(time.RFC3339Nano)
+	l.Flag = flag
+	h.Logs = append(h.Logs, l)
+}
